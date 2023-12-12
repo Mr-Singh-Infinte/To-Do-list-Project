b4817f2e2dfd4285bcf818499226de07f41a34f1
Mr-Singh-Infinite <iamrdsingh@gmail.com>
2023-12-12 21:26:07 +0530

    i will add the checked fuction in list.

--- To-Do-List.js
+++ To-Do-List.js
@@ -20,7 +20,7 @@ const listContainer = document.getElementById("list-container")
 
 listContainer.addEventListener("click", function(e){
    if(e.target.tagName === "LI"){
-      e.target.classList.toggle("list-container");
+      e.target.classList.toggle("checked");
    }
    else if (e.target.tagName === "SPAN"){
       e.target.parentElement.remove();
